ee8455e7040d954f8b7fa515633929405d9f64a8
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-01-16 17:31:28 +0530

    Create 04_Two Sum.java

---
+++ 04_Two Sum.java
@@ -0,0 +1,18 @@
+/* Problem Statement :-
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=1; i<nums.length; i++){
+            for (int j=0 ; i+j<nums.length; j++){
+                if(nums[i+j]+nums[j] == target){
+                   return new int[] {i+j,j} ;
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
