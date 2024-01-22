40bd3f1b6b848080e1cc3e9083790cdd5e9e0920
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-01-22 22:40:43 +0530

    Element with left side smaller and right side greater

---
+++ 23_Element with left side smaller and right side greater.java
@@ -0,0 +1,31 @@
+/* Problem Statement :-
+Given an unsorted array of size N. Find the first element in array such that all of its left elements are smaller and all right elements to it are greater than it.
+Note: Left and right side elements can be equal to required element. And extreme elements cannot be required element.
+*/
+
+// Answer :-
+
+class Compute {
+    public int findElement(int arr[], int n){
+        int[] leftMax = new int[n];
+        int[] rightMin = new int[n];
+
+        leftMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i - 1]);
+        }
+
+        rightMin[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMin[i] = Math.min(rightMin[i + 1], arr[i + 1]);
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] >= leftMax[i] && arr[i] <= rightMin[i]) {
+                return arr[i];
+            }
+        }
+
+        return -1;
+    }
+}
