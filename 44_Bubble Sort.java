355dd647cf3541e1698a67f460cf8865fbd743c8
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-01-29 22:39:42 +0530

    Bubble Sort

---
+++ 44_Bubble Sort.java
@@ -0,0 +1,20 @@
+/* Problem Statement :-
+Given an Integer N and a list arr. Sort the array using bubble sort algorithm.
+*/
+
+// Answer :-
+
+class Solution {
+    
+	public static void bubbleSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
