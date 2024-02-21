62793d051d1f6b4ce637fc7a08a830370f9cab48
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-02-21 23:58:07 +0530

    First Palindromic String in the Array

---
+++ 51_Find First Palindromic String in the Array.java
@@ -0,0 +1,28 @@
+//Answer :-
+
+class Solution {        
+
+    public static String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
