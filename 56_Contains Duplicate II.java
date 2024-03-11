ecc033e78be653c5aea8e6dd77533dbdd6b2aa0d
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2024-03-11 22:11:04 +0530

    Contains Duplicate II

--- 56_Contains Duplicate II.java
+++ 56_Contains Duplicate II.java
@@ -2,7 +2,7 @@
 Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 */
 
-// Answer :-
+// Answer:-
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
